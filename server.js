fb71725dd1633678425d7d41366039cb171ac841
Pedro Lopez <39785338+plopez9@users.noreply.github.com>
2020-05-19 09:38:51 -0500

    procfile

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
